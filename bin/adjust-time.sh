0dde57cacdc4c6ed95b99e2c789e1b156f385596
Guilherme Dantas <guidanoli@proton.me>
2024-12-31 20:04:43 -0300

    Fix usage string and add regex for adjust-time

--- bin/adjust-time.sh
+++ bin/adjust-time.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if [ $# -eq 0 ]
 then
     echo "Adjust modification timestamp of files based solely on their names" >&2
-    echo "Patterns: CCYY.MMDDHH and CCYYMMDD" >&2
+    echo "Patterns: CCYYMMDD.HHmmss, DD.MM.CCYY.HH.MM.ss, CCYY.MMDDHH, CCYY.MM.DD.+HH.mm.ss" >&2
     echo "Usage: $0 [FILES...]" >&2
     exit 1
 fi
@@ -23,6 +23,10 @@ do
     elif [[ "$file" =~ [0-9]{8} ]]
     then
         timestamp=`echo "$file" | sed -E 's/.*([0-9]{8}).*/\11200/'`
+        touch "$file" -t "$timestamp"
+    elif [[ "$file" =~ [0-9]{4}[^0-9][0-9]{2}[^0-9][0-9]{2}[^0-9]+[0-9]{2}[^0-9][0-9]{2}[^0-9][0-9]{2} ]]
+    then
+        timestamp=`echo "$file" | sed -E 's/.*([0-9]{4})[^0-9]([0-9]{2})[^0-9]([0-9]{2})[^0-9]+([0-9]{2})[^0-9]([0-9]{2})[^0-9]([0-9]{2}).*/\1\2\3\4\5.\6/'`
         touch "$file" -t "$timestamp"
     else
         echo "Missing: $file"
